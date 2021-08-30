13cacba48000a2a3c8a0286f480c933bbe55495c
Kei Ieki <iekikei@km.icrr.u-tokyo.ac.jp>
2021-08-30 13:52:07 +0900

    Modified skg4/genmc.pl

--- skg4/genmc.pl
+++ skg4/genmc.pl
@@ -1,16 +1,20 @@
 #!/usr/bin/perl 
 
+# Settings
+$run_min = 86119;
+$run_max = 86161;
+$data_dir = '/disk02/data7/sk5/lin';
+$analysis_dir = "$ENV{LINAC_DIR}/data";
+$skofl_env = '/usr/local/sklib_gcc4.8.5/skofl-trunk/env.csh';
+$seed1 = 45097;
+$seed2 = 21263;
+$version = "";
+$out_dir = "$ENV{LINAC_DIR}/skg4/lin$version";
+
+# Make output directories if they do not exist
 if(!-d "./card") {
     mkdir "./card";
 }
-
-#$version = "SmallFr";
-#$version = "tune";
-#$version = "noLat";
-#$version = "woMott";
-$version = "woMott_wLat";
-#$version = "woMott_woPWA";
-#$version = "woMott_livIoni";
 
 if(!-d "./script") {
     mkdir "./script";
@@ -24,90 +28,122 @@ if(!-d "./err") {
     mkdir "./err";
 }
 
-if(!-d "/disk02/lowe8/mharada/linac/sk5/skg4/out_root_$version/") {
-    mkdir "/disk02/lowe8/mharada/linac/sk5/skg4/out_root_$version/";
+if(!-d $out_dir) {
+    mkdir $out_dir;
 }
 
-
-open (CARDEXAMPLE,"Calib_LINAC.mac");
+# Template macro file
+open(CARDEXAMPLE, "Calib_LINAC.mac");
 @card_data =<CARDEXAMPLE>;
 close CARDEXAMPLE;
 
-open (INP,"/home/sklowe/linac/const/linac_sk5_runsum.dat");
-@raw_data =<INP>;
+# Runsum data
+open(INP,"$ENV{LINAC_DIR}/runsum.dat");
+@runsum =<INP>;
 close INP;
 
-$seed1 = 45097;
-$seed2 = 21263;
+# Loop for lines in run summary data
+foreach $line (@runsum){
 
-foreach $list (@raw_data){
-    chomp($list);
-    ($RunNumber,$mom,$mod,$x,$y,$z,$badrun)=split(/ /,$list);
+    # Get settings for this run
+    chomp($line);
+    ($RunNumber,$mom,$mod,$x,$y,$z,$badrun)=split(/ /,$line);
 
-    if ($RunNumber>=80000 && $mod==0 && $RunNumber<85000){
-      print " leggo $RunNumber $mom $x $y $z \n";
+    # Select runs of interest
+    if ($mod==0 && $RunNumber>=$run_min && $RunNumber<=$run_max){
+	
+      print "$RunNumber $mom $x $y $z \n";
 
+      # Loop for sub runs
       my $count;
-
       for ($count = 0; $count < 20; $count++){
-        my $filef  = sprintf("./card/0%s.%03d",$RunNumber, $count);
-        #print " card filename $filef \n";
-        &WriteCardFile($filef, $count);
-        my $files  = sprintf("./script/g4_0%s.%03d.csh",$RunNumber, $count);
-        #print " script filename $files \n";
-        &WriteScriptFile($files,$filef,$seed1);
+	  
+        # Make a card file
+        my $file_macro  = sprintf("./card/0%s.%03d",$RunNumber, $count);
+        #print " card filename $file_macro \n";
+        &WriteCardFile($file_macro, $count);
+        $seed1++; 
+        $seed2++;
 
-        $cmd = "qsub -q lowe -o out/0$RunNumber.$count -e err/0$RunNumber.$count $files";
+	# Make a script file
+        my $file_script  = sprintf("./script/g4_0%s.%03d.csh",$RunNumber, $count);
+        #print " script filename $file_script \n";
+        &WriteScriptFile($file_script,$file_macro,$seed1);
+
+	# Submit job
+        $cmd = "qsub -q lowe -o out/0$RunNumber.$count -e err/0$RunNumber.$count $file_script";
         system $cmd;
 
-        $seed1++; 
-        $seed2++; 	
       }
     }
 }
 die "Normal End";
 
 sub WriteCardFile() {
+
+  # Output file
   my $file = $_[0];
-  my $count = sprintf("%03d",$_[1]);
   open (CARD,">$file");
+
+  # Sub-run count
+  my $count = sprintf("%03d",$_[1]);
+
+  # Loop for lines in the macro file
   foreach $cardlist (@card_data){
-    chomp($cardlist);
-    if($cardlist =~ /\/SKG4\/Detector\/Material\/IDTBAParameter/){
-      $cardlist = "/SKG4/Detector/Material/IDTBAParameter $RunNumber";
+
+    # Copy one line
+    $cardlist2 = $cardlist;
+    chomp($cardlist2);
+
+    # Skip comment line
+    if ($cardlist2 =~ /^#/) {
+	print CARD "$cardlist\n";
+	next;
     }
-    if($cardlist =~ /\/SKG4\/Detector\/Material\/WaterTransparencyRun/){
-      $cardlist = "/SKG4/Detector/Material/WaterTransparencyRun $RunNumber";
+
+    # Change specific lines
+    if($cardlist2 =~ /\/SKG4\/Detector\/Material\/IDTBAParameter RUNNUM/){
+      $cardlist2 = "/SKG4/Detector/Material/IDTBAParameter $RunNumber";
     }
-    if($cardlist =~ /\/SKG4\/Global\/LowEGainRun/){
-      $cardlist = "/SKG4/Global/LowEGainRun $badrun";
+    if($cardlist2 =~ /\/SKG4\/Detector\/Material\/WaterTransparencyRun RUNNUM/){
+      $cardlist2 = "/SKG4/Detector/Material/WaterTransparencyRun $RunNumber";
     }
-    if($cardlist =~ /\/SKG4\/DarkNoise\/Run/){
-      $cardlist = "/SKG4/DarkNoise/Run $badrun";
+    if($cardlist2 =~ /\/SKG4\/Global\/LowEGainRun RUNNUM/){
+      $cardlist2 = "/SKG4/Global/LowEGainRun $badrun";
+    }   
+    if($cardlist2 =~ /\/SKG4\/DarkNoise\/Run RUNNUM/){
+      $cardlist2 = "/SKG4/DarkNoise/Run $badrun";
     }
-    if($cardlist =~ /\/SKG4\/Calibration\/LINAC\/SetRun/){
-      $cardlist = "/SKG4/Calibration/LINAC/SetRun $RunNumber";
+    if($cardlist2 =~ /\/SKG4\/Calibration\/LINAC\/SetRun RUNNUM/){
+      $cardlist2 = "/SKG4/Calibration/LINAC/SetRun $RunNumber";
     }
-    if($cardlist =~ /\/SKG4\/RootFile\/Name/){
-      $cardlist = "/SKG4/RootFile/Name /disk02/lowe8/mharada/linac/sk5/skg4/out_root_$version/lin.0${RunNumber}.$count.root";
+    if($cardlist2 =~ /\/SKG4\/RootFile\/Name OUTFILE/){
+      $cardlist2 = "/SKG4/RootFile/Name $out_dir/lin.0${RunNumber}.$count.root";
     }
-    print CARD "$cardlist\n";
+    print CARD "$cardlist2\n";
+    
   }
   close CARD;
 }
 
 sub WriteScriptFile() {
+
+    # Get arguments
     my $file = $_[0];
     my $card = $_[1];
     my $rndm = $_[2];
+
+    # Generate script file
     open (SCRIPT,">$file");
     print SCRIPT "#!/bin/csh -f\n";
-    print SCRIPT "cd /home/mharada/Lowe/LINAC/EScale/sk5_linac_tools/skg4_new/trunk/\n";
-    print SCRIPT "source /home/sklowe/skofl/r29166/env.csh\n";
-    print SCRIPT "source /home/mharada/Lowe/LINAC/EScale/sk5_linac_tools/skg4_new/trunk/G4ROOTsource.csh\n";
+    print SCRIPT "source $ENV{LINAC_DIR}/setup.csh\n";
+    print SCRIPT "cd $ENV{LINAC_DIR}/skg4/trunk/\n";
+    print SCRIPT "source $skofl_env\n";
+    print SCRIPT "source $ENV{LINAC_DIR}/skg4/trunk/G4ROOTsource.csh\n";
     print SCRIPT "hostname\n";
-    print SCRIPT "/home/mharada/Lowe/LINAC/EScale/sk5_linac_tools/skg4_new/trunk/bin/Linux-g++/SKG4 /home/mharada/Lowe/LINAC/EScale/sk5_linac_tools/skg4_new/$card $rndm\n";
+    print SCRIPT "$ENV{LINAC_DIR}/skg4/trunk/bin/Linux-g++/SKG4 $ENV{LINAC_DIR}/skg4/$card $rndm\n";
 
+    # Close script file
     close SCRIPT;
     $cmd = "chmod 755 $file";
     system $cmd;
