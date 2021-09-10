e47d5e1e61c65f971e0baaa0d74c749f1426d960
Kei Ieki <iekikei@km.icrr.u-tokyo.ac.jp>
2021-09-10 11:04:49 +0900

    Modified compare_all.cpp

--- compare/compare_all.cpp
+++ compare/compare_all.cpp
@@ -22,12 +22,6 @@ std::string const LINAC_TXT = LINAC_DIR + "/runsum.dat";
 
 Int_t main(const int argc,char *argv[]){
 
-  if (argc != 2) {
-    printf("Usage: ./compare_all [Energy mode]\n");    
-    return 1;
-  }
-  Int_t EMode = atoi(argv[1]);
-
   // Canvas Style
   gStyle->SetFrameBorderMode(0);
   gStyle->SetCanvasBorderMode(0);
@@ -65,7 +59,7 @@ Int_t main(const int argc,char *argv[]){
   Int_t linac_run, run_mode, e_mode;
   std::string line, dummy;
   std::vector<Int_t> list_run;
-  std::vector<Int_t> list_emode;
+  std::vector<Int_t> list_sel;
   Int_t nRun_sel = 0;
   while (getline(ifs_runsum, line, '\n')) {
     // Read line
@@ -73,9 +67,12 @@ Int_t main(const int argc,char *argv[]){
     ss >> linac_run >> e_mode >> run_mode;
     if(run_mode == 0){
       list_run.push_back(linac_run);
-      list_emode.push_back(e_mode);
-      if (e_mode == EMode) {
+      if (true) {
+      // if (e_mode == 8) {
+        list_sel.push_back(1);
         nRun_sel++;
+      } else {
+        list_sel.push_back(0);
       }
     }
   }
@@ -146,7 +143,7 @@ Int_t main(const int argc,char *argv[]){
       }
 
       // Fill histograms for selected runs
-      if (list_emode[iRun] == EMode) {
+      if (list_sel[iRun]) {
         Int_t bin_this = h_neff[iData]->GetEntries();
         h_neff[iData]->SetBinContent(bin_this+1, neff[iData]);
         h_neff[iData]->SetBinError(bin_this+1, neff_err[iData]);
@@ -167,7 +164,7 @@ Int_t main(const int argc,char *argv[]){
 
   }
 
-  // Draw
+  // Make canvases
   gStyle->SetPadTopMargin(0.09);
   gStyle->SetPadLeftMargin(0.08);
   gStyle->SetPadRightMargin(0.02);
@@ -178,12 +175,14 @@ Int_t main(const int argc,char *argv[]){
   gStyle->SetPadLeftMargin(0.18);
   c2->Divide(2,1);
 
+  // Draw Neff vs runnum
   c1->cd(1);
   gr_neff[1]->SetMarkerColor(2);
   gr_neff[2]->SetMarkerColor(4);
   mgr_neff->SetTitle(";Runnumber;Neffhit");
   mgr_neff->Draw("APE");
 
+  // Add legend
   TLegend *leg1 = new TLegend(0.1,0.6,0.2,0.8);
   TString str_data[N_DATA] = {"DATA", "DETSIM", "SKG4"};
   for (Int_t iData=0; iData<N_DATA; iData++) {
@@ -191,6 +190,7 @@ Int_t main(const int argc,char *argv[]){
   }
   leg1->Draw();
 
+  // Draw Neff diff vs. runnum
   c1->cd(2);
   gr_diff[1]->SetMarkerColor(2);
   gr_diff[2]->SetMarkerColor(4);
@@ -200,20 +200,23 @@ Int_t main(const int argc,char *argv[]){
   gStyle->SetTitleYOffset(0.4);
   mgr_diff->GetYaxis()->SetRangeUser(-6.0,6.0);
 
+  // Add legend
   TLatex *latex = new TLatex();
   latex->SetTextSize(0.05);
   latex->DrawTextNDC(0.15, 0.93, "*Difference of Neffhit = (MC-DATA)/DATA [%]");
 
+  // Save as PDF
   c1->Update();
   c1->Print("compare_all.pdf(");
 
+  // Draw Neff for selected runs
   c2->cd(1);
-  h_neff[0]->Draw("PE");
+  h_neff[0]->Draw("E");
   Float_t neff_max = *max_element(v_neff.begin(), v_neff.end());
-  Float_t neff_min = *max_element(v_neff.begin(), v_neff.end());
+  Float_t neff_min = *min_element(v_neff.begin(), v_neff.end());
   h_neff[0]->GetYaxis()->SetRangeUser(neff_min*0.95, neff_max*1.03);
   for (Int_t iData=1; iData<N_DATA; iData++) {
-    h_neff[iData]->Draw("PE same");
+    h_neff[iData]->Draw("E same");
   }
   TLegend *leg2 = new TLegend(0.65,0.7,0.95,0.85);
   for (Int_t iData=0; iData<N_DATA; iData++) {
@@ -221,19 +224,15 @@ Int_t main(const int argc,char *argv[]){
   }
   leg2->Draw();
 
+  // Draw Neff ratio for selected runs
   c2->cd(2); 
-  h_ratio[1]->Draw("PE");
+  h_ratio[1]->Draw("E");
   h_ratio[1]->SetMaximum(1.03);
   h_ratio[1]->SetMinimum(0.97);
-  h_ratio[2]->Draw("PE same");
-
-  latex->SetTextSize(0.1);
-  latex->DrawTextNDC(0.6, 0.8, Form("%d MeV", EMode));
-  latex->SetTextSize(0.05);
+  h_ratio[2]->Draw("E same");
 
   // Open output file
   std::ofstream ofs("corepmt.dat", std::ios::app);
-  ofs << EMode;
 
   // Calculate and draw mean and RMS
   for (Int_t iData=1; iData<N_DATA; iData++) {
@@ -245,8 +244,9 @@ Int_t main(const int argc,char *argv[]){
     Float_t ratio_rms = sqrt(ratio_sq_sum / v_ratio[iData].size() - ratio_mean * ratio_mean);  
 
     // Draw
+    latex->SetTextSize(0.04);
     latex->SetTextColor(color_data[iData]);
-    latex->DrawTextNDC(0.35, 0.35-0.05*iData, Form("AVE = %5.4lf  RMS = %5.4lf", ratio_mean, ratio_rms));
+    latex->DrawTextNDC(0.30, 0.35-0.05*iData, Form("%s AVE = %5.4lf  RMS = %5.4lf", str_data[iData].Data(), ratio_mean, ratio_rms));
 
     // Write
     printf("AVE (%s)\t\t:=  %5.4lf  RMS = %5.4lf\n", str_data[iData].Data(), ratio_mean, ratio_rms);
@@ -268,8 +268,6 @@ Int_t main(const int argc,char *argv[]){
   // Output PDF
   c2->Update();
   c2->Print("compare_all.pdf)");
-
-  c2->SaveAs("test.root");
 
   return 1; 
 }
